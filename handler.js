079ab5d6fe86956d3bb3bc951455f845d797745c
Wenjuan Dai <demi.dai@invenco.com>
2020-01-29 08:47:53 +1300

    add all

---
+++ handler.js
@@ -0,0 +1,85 @@
+'use strict';
+
+module.exports.sendReminderMinutes = (event, context, callback) => {
+
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-west-2'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
+
+    var emailHtml = fs.readFileSync('./minutesReminder.html', 'utf-8');
+
+    var toAndFromAdress = 'superdaiwenjuan@gmail.com'
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: emailHtml
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Remember to continue helping the Woof Garden in your Pluralsight course!"
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress,
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack);
+        // successful response
+        else callback(null, data);
+    });
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-west-2'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
+
+    var emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+    var toAndFromAdress = 'superdaiwenjuan@gmail.com'
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAdress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8",
+                    Data: emailHtml
+                },
+                Text: {
+                    Charset: "UTF-8",
+                    Data: "Here's a weekend Remember that puppies are adorable!!"
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Woof Garden Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAdress],
+        Source: toAndFromAdress,
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack);
+        // successful response
+        else callback(null, data);
+    });
+};
